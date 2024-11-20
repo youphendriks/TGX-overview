f777d48d07528820c76f80080f8e195103d647fe
youphendriks <youp@youphendriks>
2024-11-20 16:51:02 +0100

    Fixed function naming

--- scripts/create-readme.py
+++ scripts/create-readme.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
-import os
 import json
-import markdown
-from bs4 import BeautifulSoup
+import os
 
-# 0. Call functions
+import markdown
+
+
 def main():
+    """0. Call functions"""
     # 1. Import projectlist
-    projectlist = importProjectlist("scripts/project_list.json")
+    projectlist = import_projectlist("scripts/project_list.json")
     # 2. Import taglist
-    taglist = importTaglist("scripts/tag_list.json")
+    taglist = import_taglist("scripts/tag_list.json")
     # 3. Extract data
     for project in projectlist:
-        extractData(project, taglist)
+        extract_data(project, taglist)
         # 4. Format Json
         # 5. Create Json
     # 6. Create HTML
-    createHTML(projectlist)
+    create_html(projectlist)
 
-# 1. Import projectlist
-def importProjectlist(path):
-    with open(path) as f:
+
+def import_projectlist(path):
+    """1. Import projectlist"""
+    with open(path, encoding="utf-8") as f:
         projectlist = json.load(f)
     for project in projectlist:
         name = project["name"]
@@ -30,59 +32,77 @@ def importProjectlist(path):
         os.system("wget -O db/" + name + "/README.md " + rawlink)
     return projectlist
 
-# 2. Import taglist
-def importTaglist(path):
-    with open(path) as f:
+
+def import_taglist(path):
+    """2. Import taglist"""
+    with open(path, encoding="utf-8") as f:
         taglist = json.load(f)
     print("Taglist:")
     print(taglist)
     return taglist
 
-# 3.Extract data
-def extractData(project, taglist):
+
+def extract_data(project, taglist):
+    """3. Extract data"""
     name = project["name"]
-    projectlib = open(("db/" + name + "/README.md"), 'r').read()
+    path = "db/" + name + "/README.md"
+    project_readme = open(path, "r", encoding="utf-8").read()
     tagdict = {}
     for tag in taglist:
         try:
-            split1 = projectlib.split("<!--"+tag+"-->")
-            split2 = split1[1].split("<!--/"+tag+"-->")
-            tagdict[tag]=split2[0]
+            split1 = project_readme.split("<!--" + tag + "-->")
+            split2 = split1[1].split("<!--/" + tag + "-->")
+            tagdict[tag] = split2[0]
         except:
-            tagdict[tag]="404 data not found"
+            tagdict[tag] = "404 data not found"
     # print(tagdict)
-    formatJson(tagdict, name)
+    format_json(tagdict, name)
 
-# 4. Format Json
-def formatJson(tagdict, name):
-    formatTagdict = {}
+
+def format_json(tagdict, name):
+    """4. Format Json"""
+    format_tagdict = {}
     for tag in tagdict:
         html = markdown.markdown(tagdict[tag])
-        html = html.replace("\n"," ")
-        html = html.replace("\t"," ")
-        formatTagdict[tag] = html
-    print("formatTagdict:")
-    print(formatTagdict)
-    createJson(formatTagdict, name)
+        html = html.replace("\n", " ")
+        html = html.replace("\t", " ")
+        format_tagdict[tag] = html
+    print("format_tagdict:")
+    print(format_tagdict)
+    create_json(format_tagdict, name)
 
-# 5. Create Json
-def createJson(formatTagdict, name):
-    with open(('db/' + name + '/' + name+'.json'), 'w') as f:
-        json.dump(formatTagdict, f)
 
-# 6. Create HTML
-def createHTML(projectlist):
-    template = open(("scripts/template-software.html"), 'r').read()
+def create_json(format_tagdict, name):
+    """# 5. Create Json"""
+    path = "db/" + name + "/" + name + ".json"
+    with open(path, "w", encoding="utf-8") as f:
+        json.dump(format_tagdict, f)
+
+
+def create_html(projectlist):
+    """# 6. Create HTML"""
+    path = "scripts/template-software.html"
+    template = open(path, "r", encoding="utf-8").read()
     accordion = ""
     for project in projectlist:
         name = project["name"]
-        projectHTML= open(("db/" + name + "/" + name + ".json"), 'r').read()
-        projectHTML = json.loads(projectHTML)
-        accordion+='<div class="col"><button class="accordion"><h2>' + projectHTML["Title"] + '</h2><p id="statement">' + projectHTML["Statement"] + '</p></button><div class="panel"><p id="description"><h4><b>Description:</b></h4>' + projectHTML["Description"] + '</div></div>'
+        path = "db/" + name + "/" + name + ".json"
+        projet_html = open(path, "r", encoding="utf-8").read()
+        projet_html = json.loads(projet_html)
+        accordion += (
+            '<div class="col"><button class="accordion"><h2>'
+            + projet_html["Title"]
+            + '</h2><p id="statement">'
+            + projet_html["Statement"]
+            + '</p></button><div class="panel"><p id="description"><h4><b>Description:</b></h4>'
+            + projet_html["Description"]
+            + "</div></div>"
+        )
     split = template.split('id="bodyContainer">')
-    html = split[0] + 'id="bodyContainer">'+ accordion + split[1]
-    with open(('pages/software.html'), 'w') as f:
+    html = split[0] + 'id="bodyContainer">' + accordion + split[1]
+    with open(("pages/software.html"), "w", encoding="utf-8") as f:
         f.write(html)
     print("fin.")
+
 
 main()
